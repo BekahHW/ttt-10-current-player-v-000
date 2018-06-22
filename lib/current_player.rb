18257408a7170790c166a33bf366bc93a9cc9b9b
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-06-22 10:24:09 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,9 +2,9 @@ def turn_count(board)
 counter = 0
   board.each|empty_space| 
   counter += 1
-   if counter == 9
-   break 
- end
+#   if counter == 9
+#   break 
+# end
 end
 
 def current_player(board)
