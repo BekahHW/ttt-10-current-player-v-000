b8881ee5a88379019880b563a44f2c32c61d0efa
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-06-22 10:42:45 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,21 +1,12 @@
 def turn_count(board)
-# counter = 0
-#   board.each do |player|
-#     if player == "X" || "O"
-#   counter += 1
-# end
-# end
-# counter
-# end
-  counter=0
- board.each do |turn|
-   if turn == 'X' || turn == 'O'
-     counter += 1
+counter = 0
+  board.each do |player|
+    if player == "X" || player == "O"
+  counter += 1
 end
 end
 counter
 end
-
 
 def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
